38114936f80bd0f102bdde20fc8b70ba25e2dce8
Miraj Mahmud <mirajmahmud417@gmail.com>
2024-04-18 23:45:53 +0100

    Updated pytest

--- test/test_pipleine_data_core.py
+++ test/test_pipleine_data_core.py
@@ -2,13 +2,22 @@ import pytest
 import pandas as pd 
 import numpy as np 
 
-df = pd.read_csv(r"C:\Users\Miraj\Downloads\sales_data_sample.csv",encoding="latin-1")
+@pytest.fixture
+def df():
+  df = pd.read_csv(r"C:\Users\Miraj\Downloads\sales_data_sample.csv",encoding="latin-1")
+  return  df
 
 #check if column exist 
-def test_col_exists():
+def test_col_exists(df):
   name = "SALES"
   assert name in df.columns
   
 #check for NaN values
-def test_null_check():
+def test_null_check(df):
   assert np.where(df["SALES"].isnull())
+  
+#check values in range
+def test_range_val_str(df):
+  assert df["SALES"].between(0,10000).any()
+  
+  
